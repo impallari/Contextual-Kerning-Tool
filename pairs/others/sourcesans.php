fb17a7c7c73aee0eb22ed739b937ff697727699f
Pablo Impallari <impallari@gmail.com>
2016-10-10 02:54:19 -0300

    update

---
+++ pairs/others/sourcesans.php
@@ -0,0 +1,85 @@
+<?php
+
+# Human readable name
+$font_name = "Source Sans"; 
+
+# Lower to Lower
+# -------------------------
+$pairs_core_lower_to_lower = array(
+"ba", "bf", "bs", "bv", "bw", "bx", "by", "bz", 
+"ca", "cc", "cd", "ce", "cg", "co", "cq", "cx", 
+"ea", "ef", "eg", "et", "ev", "ew", "ex", "ey", 
+"fa", "fc", "fd", "fe", "fg", "fj", "fo", "fq", "fs", "fu", "fv", "fx", "fz", 
+"ga", "gc", "gd", "ge", "gj", "go", "gq", "gv", "gw", "gy", "gz", 
+"ka", "kc", "kd", "ke", "kg", "kj", "ko", "kq", "kt", "ku", "kx", "kz", 
+"oa", "of", "os", "ov", "ow", "ox", "oy", "oz", 
+"pa", "pf", "ps", "pv", "pw", "px", "py", "pz", 
+"ra", "rc", "rd", "re", "rg", "ro", "rq", "rs", "rv", "rw", "ry", "rz", 
+"ta", "tc", "td", "te", "tg", "to", "tq", 
+"va", "vc", "vd", "ve", "vj", "vo", "vq", "vz", 
+"wa", "wc", "wd", "we", "wj", "wo", "wq", "wz", 
+"xa", "xc", "xd", "xe", "xo", "xq", "xy", 
+"ya", "yc", "yd", "ye", "yj", "yo", "yq", "yz", 
+"za", "zc", "zd", "ze", "zo", "zq", "zu", "zv", "zy"
+		);
+$pairs_additional_lower_to_lower = array();
+											
+# Upper to Upper	
+# -------------------------												
+$pairs_core_upper_to_upper = array(
+"AC", "AG", "AO", "AQ", "AT", "AU", "AV", "AX", "AY", 
+"BJ", "BS", "BT", "BV", "BW", "BX", "BY", "BZ", 
+"CC", "CG", "CJ", "CO", "CQ", "CS", "CT", "CU", "CV", "CW", "CX", "CY", "CZ", 
+"DA", "DJ", "DT", "DV", "DX", "DY", "DZ", 
+"FA", "FC", "FG", "FJ", "FO", "FQ", "FS", "FV", "FW", "FX", "FY", "FZ", 
+"GA", "GT", "GV", "GW", 
+"JJ", 
+"KA", "KC", "KG", "KO", "KQ", "KS", "KT", "KU", "KV", "KW", "KY", 
+"LA", "LC", "LG", "LO", "LQ", "LS", "LT", "LU", "LV", "LW", "LY", 
+"OA", "OJ", "OT", "OV", "OX", "OY", "OZ", 
+"PA", "PJ", "PS", "PT", "PX", "PY", "PZ", 
+"QA", "QJ", "QT", "QV", "QX", "QY", "QZ", 
+"RA", "RJ", "RS", "RT", "RV", "RX", "RY", "RZ", 
+"SJ", "SS", "ST", "SY", 
+"TA", "TC", "TG", "TJ", "TO", "TQ", "TS", "TX", "TY", "TZ", 
+"UA", "UJ", "US", "UV", "UX", "UY", 
+"VA", "VC", "VG", "VJ", "VO", "VQ", "VS", "VU", "VZ", 
+"WJ", 
+"XA", "XC", "XG", "XJ", "XO", "XQ", "XS", "XT", "XU", 
+"YA", "YC", "YG", "YJ", "YO", "YQ", "YS", "YT", "YU", "YZ", 
+"ZC", "ZG", "ZJ", "ZO", "ZQ", "ZS", "ZU", "ZY", "ZZ"
+		);	
+$pairs_additional_upper_to_upper = array();
+														  
+# Upper to Lower
+# -------------------------
+$pairs_core_upper_to_lower = array(
+"Af", "At", "Av", "Aw", "Ay", 
+"Bt", "Bv", "Bw", "Bx", "By", 
+"Cg", "Ct", "Cu", "Cv", "Cw", "Cy", 
+"Dz", 
+"Fa", "Fc", "Fd", "Fe", "Fg", "Fm", "Fn", "Fo", "Fp", "Fq", "Fr", "Fs", "Ft", "Fu", "Fv", "Fw", "Fx", "Fy", "Fz", 
+"Kc", "Kd", "Ke", "Kj", "Ko", "Kq", "Kt", "Ku", "Kv", "Kw", "Kx", "Ky", "Kz", 
+"Lc", "Ld", "Le", "Lf", "Lg", "Lo", "Lq", "Lt", "Lu", "Lv", "Lw", "Ly", 
+"Oz", 
+"Pa", "Pc", "Pd", "Pe", "Pg", "Po", "Pq", "Ps", "Px", "Pz", 
+"Qz", 
+"Ra", "Rc", "Rd", "Re", "Rg", "Ro", "Rq", "Rx", "Rz", 
+"Sz", 
+"Ta", "Tc", "Td", "Te", "Tf", "Tg", "Tm", "Tn", "To", "Tp", "Tq", "Tr", "Ts", "Tt", "Tu", "Tv", "Tw", "Tx", "Ty", "Tz", 
+"Ua", "Ug", "Us", "Ux", 
+"Va", "Vc", "Vd", "Ve", "Vg", "Vm", "Vn", "Vo", "Vp", "Vq", "Vr", "Vs", "Vu", "Vv", "Vw", "Vx", "Vy", "Vz", 
+"Wa", "Wg", "Wz", 
+"Xa", "Xc", "Xd", "Xe", "Xf", "Xo", "Xq", "Xt", "Xu", "Xv", "Xw", "Xx", "Xy", "Xz", 
+"Ya", "Yc", "Yd", "Ye", "Yg", "Ym", "Yn", "Yo", "Yp", "Yq", "Yr", "Ys", "Yt", "Yu", "Yv", "Yw", "Yx", "Yy", "Yz", 
+"Za", "Zc", "Zd", "Ze", "Zf", "Zg", "Zo", "Zq", "Zt", "Zu", "Zv", "Zw", "Zy"
+		);
+$pairs_additional_upper_to_lower = array();														 	
+
+# Merge and send pairs list to app
+# -------------------------
+$pairs_upper_to_upper = array_merge($pairs_core_upper_to_upper, $pairs_additional_upper_to_upper);
+$pairs_lower_to_lower = array_merge($pairs_core_lower_to_lower, $pairs_additional_lower_to_lower);
+$pairs_upper_to_lower = array_merge($pairs_core_upper_to_lower, $pairs_additional_upper_to_lower);
+
+?>
